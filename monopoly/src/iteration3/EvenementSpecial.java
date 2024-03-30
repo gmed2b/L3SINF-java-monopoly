c090be20b42c95b7b639d82345c436ac845bf456
antocreadev <anthonymenghi@protonmail.com>
2024-03-30 15:55:58 +0100

    add: action apres avoir reculer ou avancer

--- monopoly/src/iteration3/EvenementSpecial.java
+++ monopoly/src/iteration3/EvenementSpecial.java
@@ -19,7 +19,7 @@ public class EvenementSpecial extends Case {
     public void action(Joueur joueur, Plateau plateau) {
         if (this.type == TypeEvenement.CHANCE || this.type == TypeEvenement.COMMUNAUTE) {
             Carte carte = this.type == TypeEvenement.CHANCE ? plateau.getCartesChance() : plateau.getCartesCommunaute();
-            System.out.println("Carte tirée : " + carte.texte);
+            System.out.println("Carte tirée : " + carte.texte); // à mettre dans le CLI
             if (carte.type == Carte.TypeCarte.GAGNER) {
                 joueur.crediter(carte.effet);
                 System.out.println("Vous avez gagné " + carte.effet + "€ !"); // à mettre dans le CLI
@@ -33,9 +33,15 @@ public class EvenementSpecial extends Case {
             } else if (carte.type == Carte.TypeCarte.AVANCER) {
                 joueur.seDeplacer(carte.effet, plateau.getTaille());
                 System.out.println("Vous avancez de " + carte.effet + " cases !"); // à mettre dans le CLI
+                Case caseActuelle = plateau.getCase(joueur.getPosition());
+                Cli.afficherCase(caseActuelle);
+                caseActuelle.action(joueur, plateau);
             } else if (carte.type == Carte.TypeCarte.RECULER) {
                 joueur.seDeplacer(-carte.effet, plateau.getTaille());
                 System.out.println("Vous reculez de " + carte.effet + " cases !"); // à mettre dans le CLI
+                Case caseActuelle = plateau.getCase(joueur.getPosition());
+                Cli.afficherCase(caseActuelle);
+                caseActuelle.action(joueur, plateau);
             }
         }
 
